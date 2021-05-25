3a940160eea54be1ee0a21b7a5eb15585f88ddb3
Dmytro Abram <dmytro.study@gmail.com>
2021-05-25 14:54:13 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.a9a34fb893706af315193794a7a75692.js"
 );
 
 self.addEventListener('message', (event) => {
